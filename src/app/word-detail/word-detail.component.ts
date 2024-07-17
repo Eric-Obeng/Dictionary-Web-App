d35a731c63f286ea75e0b30ee564242ec20b45a4
Eric-Obeng <obengeric.3958@gmail.com>
2024-07-17 10:45:07 +0000

    all needed components created

---
+++ src/app/word-detail/word-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-word-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './word-detail.component.html',
+  styleUrl: './word-detail.component.css'
+})
+export class WordDetailComponent {
+
+}
