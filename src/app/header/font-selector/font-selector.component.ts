8d29d69b7c6c006acede3f1d1e9c74543ab005a2
Eric-Obeng <obengeric.3958@gmail.com>
2024-07-17 11:45:34 +0000

    font selector component created

---
+++ src/app/header/font-selector/font-selector.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-font-selector',
+  standalone: true,
+  imports: [],
+  templateUrl: './font-selector.component.html',
+  styleUrl: './font-selector.component.css'
+})
+export class FontSelectorComponent {
+
+}
