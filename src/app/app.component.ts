f23705b1f5542d54c73d3d12d46f5201af776691
Eric-Obeng <obengeric.3958@gmail.com>
2024-07-17 11:01:01 +0000

    imported all components in the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { SearchComponent } from './search/search.component';
+import { WordDetailComponent } from "./word-detail/word-detail.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, SearchComponent, WordDetailComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
