63df8f5867b6224303218788c9a66d5602ad786d
Eric-Obeng <obengeric.3958@gmail.com>
2024-07-18 23:59:38 +0000

    dictionary service updated

--- src/app/service/dictionary.service.ts
+++ src/app/service/dictionary.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DictionaryService {
+  private url = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
+  private searchResultsSubject = new BehaviorSubject<any>(null);
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  fetchWord(query: string): Observable<any> {
+    return this.http
+      .get(`${this.url}${query}`)
+      .pipe(tap((results) => this.searchResultsSubject.next(results)));
+  }
+
+  getSearchResults(): Observable<any> {
+    return this.searchResultsSubject.asObservable();
+  }
+
+  testSearch() {
+    this.fetchWord('keybord');
+  }
 }
