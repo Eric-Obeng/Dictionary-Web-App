905c062c909379f6c171c84426b629a36ea02351
Eric-Obeng <obengeric.3958@gmail.com>
2024-07-19 08:22:03 +0000

    feat: maning component updated

--- src/app/meaning/meaning.component.ts
+++ src/app/meaning/meaning.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-meaning',
   standalone: true,
   imports: [],
   templateUrl: './meaning.component.html',
-  styleUrl: './meaning.component.css'
+  styleUrl: './meaning.component.css',
 })
 export class MeaningComponent {
-
+  @Input() meanings: any[] = [];
 }
