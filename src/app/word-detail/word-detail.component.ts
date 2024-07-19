592ae621b5299ce9520549d53fc7f8598877d8b6
Eric-Obeng <obengeric.3958@gmail.com>
2024-07-19 08:22:52 +0000

    feat: word detail component updated

--- src/app/word-detail/word-detail.component.ts
+++ src/app/word-detail/word-detail.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MeaningComponent } from '../meaning/meaning.component';
+import { SynonymComponent } from '../synonym/synonym.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-word-detail',
   standalone: true,
-  imports: [],
+  imports: [MeaningComponent, SynonymComponent, CommonModule],
   templateUrl: './word-detail.component.html',
-  styleUrl: './word-detail.component.css'
+  styleUrl: './word-detail.component.css',
 })
 export class WordDetailComponent {
+  @Input() wordDetails: any;
 
+  playAudio() {
+    const audioUrl = this.getAudioUrl();
+    if (audioUrl) {
+      const audio = new Audio(audioUrl);
+      audio.play();
+    } else {
+      console.log('No audio URL found');
+    }
+  }
+
+  getAudioUrl(): string | null {
+    if (this.wordDetails && this.wordDetails.phonetics) {
+      for (const phonetic of this.wordDetails.phonetics) {
+        if (phonetic.audio) {
+          return phonetic.audio;
+        }
+      }
+    }
+    return null;
+  }
 }
