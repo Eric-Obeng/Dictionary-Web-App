5fa4a45b9f0bd0df4f04c5d426ea52fef93d2250
Eric-Obeng <obengeric.3958@gmail.com>
2024-07-19 08:22:26 +0000

    feat: word component updated

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { DictionaryService } from '../service/dictionary.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.css'
+  styleUrl: './search.component.css',
 })
 export class SearchComponent {
+  word: string = '';
+  @Output() wordData = new EventEmitter<any>();
+  errorMessage: string = '';
 
+  constructor(private dictionaryService: DictionaryService) {}
+
+  onSearch() {
+    if (this.word.trim()) {
+      this.dictionaryService.fetchWord(this.word.trim()).subscribe(
+        (data) => {
+          this.wordData.emit(data[0]);
+          console.log(data[0]);
+        },
+        (error) => {
+          console.log('Error fetching word data', error);
+        }
+      );
+    } else {
+      this.errorMessage = 'Whoops, can’t be empty…';
+    }
+  }
+
+  onInput() {
+    if (this.errorMessage) {
+      this.errorMessage = '';
+    }
+  }
 }
