21eb72c4721340e8f528894f5b9d06a61ec1525d
Eric-Obeng <obengeric.3958@gmail.com>
2024-07-19 08:20:32 +0000

    feat: header component updated

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FontSelectorComponent } from './font-selector/font-selector.component';
+import { ThemeComponent } from './theme/theme.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [FontSelectorComponent, ThemeComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  @Output() fontChange = new EventEmitter<string>();
 
+  onFontChange(font: string) {
+    this.fontChange.emit(font);
+  }
 }
