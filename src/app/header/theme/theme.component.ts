60e1b905cd82d36f4105fcc5f013d6e40ce0c92e
Eric-Obeng <obengeric.3958@gmail.com>
2024-07-19 08:21:40 +0000

    feat: theme component updated

--- src/app/header/theme/theme.component.ts
+++ src/app/header/theme/theme.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  HostBinding,
+  Output,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-theme',
   standalone: true,
   imports: [],
   templateUrl: './theme.component.html',
-  styleUrl: './theme.component.css'
+  styleUrls: ['./theme.component.css'],
 })
-export class ThemeComponent {
+export class ThemeComponent implements OnInit {
+  @Output() themeChange = new EventEmitter<boolean>();
 
+  isDarkMode: boolean = false;
+
+  @HostBinding('class.dark-mode') get darkMode() {
+    return this.isDarkMode;
+  }
+
+  ngOnInit(): void {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+      this.isDarkMode = true;
+      document.body.classList.add('dark-mode');
+    }
+  }
+
+  toggleTheme(event: Event) {
+    const target = event.target as HTMLElement;
+    if (
+      target.classList.contains('toggle') ||
+      target.classList.contains('toggle-btn')
+    ) {
+      this.isDarkMode = !this.isDarkMode;
+      if (this.isDarkMode) {
+        document.body.classList.add('dark-mode');
+        localStorage.setItem('theme', 'dark');
+      } else {
+        document.body.classList.remove('dark-mode');
+        localStorage.setItem('theme', 'light');
+      }
+      this.themeChange.emit(this.isDarkMode);
+    }
+  }
 }
