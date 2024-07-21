4b8c6ee170555dd9b5ee1d906d237e60ba531942
Eric-Obeng <obengeric.3958@gmail.com>
2024-07-21 04:44:45 +0000

    font selector component styles

--- src/app/header/font-selector/font-selector.component.ts
+++ src/app/header/font-selector/font-selector.component.ts
@@ -1,17 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-font-selector',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './font-selector.component.html',
-  styleUrl: './font-selector.component.css'
+  styleUrl: './font-selector.component.css',
 })
 export class FontSelectorComponent {
+  fonts: string[] = ['Sans Serif', 'Serif', 'Mono'];
+  selectedFont: string = 'Sans Serif';
+  isDropdownOpen: boolean = false;
+
   @Output() fontChange = new EventEmitter<string>();
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  selectFont(font: string) {
+    this.selectedFont = font;
+    this.isDropdownOpen = false;
+    this.fontChange.emit(font);
+  }
 
   onFontChange(event: Event) {
     const target = event.target as HTMLSelectElement;
-    this.fontChange.emit(target.value)
+    this.selectFont(target.value);
   }
 }
